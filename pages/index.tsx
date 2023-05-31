38e5754ad1fa2dcc4b48a3b5315c3254ff473a1d
zihaooo9 <chngzihao@gmail.com>
2023-05-31 18:02:47 +0800

    Update Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,13 +29,25 @@ export default function Home() {
         <h1 className="bg-clip-text py-7 text-6xl font-extrabold tracking-wide sm:text-6xl mt-8">
           URL_SHORTENER
         </h1>
-        <input
-          type="text"
-          value={originalUrl}
-          onChange={handleUrlChange}
-          placeholder="Enter URL"
-        />
-        <button className="bg-sky-500/100" onClick={handleShorten}>Shorten</button>
+        <div className="container mx-auto max-w-lg flex flex-row py-2 ">
+          <div className="flex-auto	w-full">
+            <input
+              type="text"
+              className="border-4 border-black rounded-lg w-full h-14"
+              value={originalUrl}
+              onChange={handleUrlChange}
+              placeholder="Enter URL"
+            />
+          </div>
+          <div className="flex-auto	pl-2">
+            <button
+              className="btn bg-sky-500/100 h-14 border-solid border-4 border-transparent rounded	font-extrabold text-md text-center"
+              onClick={handleShorten}
+            >
+              SHORTEN
+            </button>
+          </div>
+        </div>
         {shortenedUrl && (
           <div>
             <a href={shortenedUrl} target="_blank" rel="noopener noreferrer">
